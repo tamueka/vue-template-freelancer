416e045b439cba5cea5326cb84544af0e0695bde
tamueka <tamueka@hotmail.com>
2020-04-29 04:16:52 +0200

    upload project freelancer theme

---
+++ src/components/core/AppBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app-bar
+    v-scroll="onScroll"
+    :color="!isScrolling ? 'transparent' : 'white'"
+    fixed
+    flat
+  >
+    <v-slide-x-transition>
+      <v-img
+        v-if="showLogo"
+        :src="require('@/assets/logo.png')"
+        class="shrink"
+        contain
+        height="50"
+      />
+    </v-slide-x-transition>
+
+    <v-spacer />
+
+    <social-media />
+
+    <base-btn
+      class="ml-3"
+      large
+    >
+      Sig In
+
+      <v-icon
+        right
+        small
+      >
+        mdi-account
+      </v-icon>
+    </base-btn>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'CoreAppBar',
+
+    components: {
+      SocialMedia: () => import('@/components/SocialMedia'),
+    },
+
+    data: () => ({
+      showLogo: false,
+      isScrolling: false,
+    }),
+
+    methods: {
+      onScroll () {
+        const offset = window.pageYOffset
+        this.isScrolling = offset > 50
+        this.showLogo = offset > 200
+      },
+    },
+  }
+</script>
